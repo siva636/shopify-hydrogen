59fa2a512cfde9c02e32509dd462ad25c19cc479
siva <siva636@gmail.com>
2025-12-25 06:02:45 +0530

    Apply home page improvements

--- app/routes/($locale)._index.tsx
+++ app/routes/($locale)._index.tsx
@@ -83,34 +83,23 @@ function FeaturedCollection({
       {image && (
         <div className="featured-collection-image">
           <div className="relative flex justify-center items-center">
-            <Image data={image} sizes="100vw" />
+            <Image className='md:hidden' data={image} sizes="100vw" aspectRatio='1/1' crop='right' />
+            <Image className='hidden md:block' data={image} sizes="100vw" />
             <div className="absolute text-center">
               <div className="py-2 text-5xl md:text-6xl lg:text-7xl lg:font-bold text-white text-center">
                 <div>Elevate your lifestyle today.</div>
                 <div>Tomorrow means never.</div>
               </div>
-              {/* <button className="m-6 bg-white text-black py-4 px-8 text-2xl font-bold tracking-wide border-2 border-black rounded-lg ransition-all duration-300 hover:bg-black hover:text-white hover:shadow-lg cursor-pointer">
-                Elevate now
-              </button> */}
-              <div className="md:hidden">
-                <Button size="lg" className='h-[56px] px-[16px] rounded-[16px]'>
-                  <TrendingUpIcon />  <span className='font-bold'>Elevate now</span>
-                </Button>
-              </div>
-              <div className="hidden md:block">
-                <Button size="lg" className='h-[80px] px-[30px] rounded-[20px] font-bold text-xl'>
-                  <TrendingUpIcon className='m-1' />  Elevate now
-                </Button>
-              </div>
-              {/* <div className="hidden lg:block">
-                {M3ExtendedFabLg({ label: 'Elevate now', icon: 'trending_up' })}
-              </div> */}
+
+              <Button variant="outline" size="lg" className='h-[65px] rounded-[20px] font-bold text-xl'>
+                <TrendingUpIcon className='m-1' />  <span className='pr-3'> Elevate now</span>
+              </Button>
+
             </div>
           </div>
         </div>
       )
       }
-      {/* <h1>{collection.title}</h1> */}
     </Link >
   );
 }
